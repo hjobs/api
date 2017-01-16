42dd997eadb9f3b68b744a902503fd4044c80a49
Edmund To <edmundtfy@gmail.com>
2017-01-16 19:46:55 +0800

    authentication before action and routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/:iam' do
     match '/authenticate' => 'authentication#authenticate', via: :post
-    get '/getAllQuestions', to: 'questions#question_trial'
   end
 end
