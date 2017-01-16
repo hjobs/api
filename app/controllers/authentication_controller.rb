77e5942e18663be5a42a87ccca3d01be496818bc
Edmund To <edmundtfy@gmail.com>
2017-01-16 19:51:52 +0800

    added logging in authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
     @command = AuthenticateUser.call(params[:iam], params[:email], params[:password])
 
     if @command.success?
-      # logger.debug "inside @command.success?, @command.reult/auth_token = " + @command.result
+      logger.debug "inside @command.success?, @command.reult/auth_token = " + @command.result
       if params[:iam] == 'employee'
         @employee = Employee.find_by(email: params[:email])
         render json: { employee: @employee, auth_token: @command.result}, status: :created, location: @employee
@@ -16,6 +16,7 @@ class AuthenticationController < ApplicationController
       end
 
     else
+      logger.debug "inside @command.success else, cannot log in"
       render json: { error: @command.errors }, status: :unauthorized
     end
   end
