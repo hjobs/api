37861b7a5f4233c1b48dc854cd5cf51c416c8654
Edmund <edmundtfy@gmail.com>
2017-01-27 21:23:12 +0800

    migration bug on organisations --> orgs

--- db/migrate/20170126122840_add_email_to_organisation.rb
+++ db/migrate/20170126122840_add_email_to_organisation.rb
@@ -1,6 +1,6 @@
 class AddEmailToOrganisation < ActiveRecord::Migration[5.0]
   def change
-    add_column :organisations, :email, :string
-    add_index :organisations, :email
+    add_column :orgs, :email, :string
+    add_index :orgs, :email
   end
 end
