8d780fd291791027d5a01fc4cb51aec1c3f957e1
Edmund <edmundtfy@gmail.com>
2017-02-25 16:09:36 +0800

    improved sh access

---
+++ zIgnore/shDev.sh
@@ -0,0 +1,2 @@
+chmod 600 zIgnore/pem_dev.pem
+ssh -i zIgnore/pem_dev.pem 'ubuntu'@'dev.hjobs.hk'
--- zIgnore/shProd.sh
+++ zIgnore/shProd.sh
@@ -1,2 +1,2 @@
 chmod 600 zIgnore/pem_prod.pem
-ssh -i zIgnore/pem_prod.pem 'ubuntu'@'52.221.40.15'
+ssh -i zIgnore/pem_prod.pem 'ubuntu'@'api.hjobs.hk'
