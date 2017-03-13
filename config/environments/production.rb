bb860e8c72b706edd552aedce154ae9ec432ce3e
Edmund <edmundtfy@gmail.com>
2017-03-13 22:50:09 +0000

    log everything in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.log_level = :info
+    config.log_level = :debug
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   # end
 
