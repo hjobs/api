c5ac30f74ff2b40c166b059e69ca0de96f93f66e
Edmund <edmundtfy@gmail.com>
2017-04-17 21:15:23 +0800

    bug fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    if session[:user_id] && user = Employee.find(session[:user_id])
+    if session[:user_id] && Employee.find(session[:user_id])
       # Means our user is signed in. Add the authorization to the user
+      user = Employee.find(session[:user_id])
       user.add_provider(auth_hash)
 
       render :json => user
