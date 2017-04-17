4e2b04f063029290dbb97bb23343abade5965910
Edmund <edmundtfy@gmail.com>
2017-04-17 19:33:44 +0800

    get rid of localhost:9080 hardcoded shit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.full_host = "http://localhost:9080"
+# OmniAuth.config.full_host = "http://localhost:9080"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
@@ -8,8 +8,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :scope => "userinfo.email,userinfo.profile",
     :prompt => "select_account",
     :image_aspect_ratio => "square",
-    :image_size => 50,
-    :redirect_uri => "http://localhost:9080/auth/google/callback"
+    :image_size => 50
   }
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
