524aaa9b165d849df0d8c049ead7ea6be1a80e89
Edmund <edmundtfy@gmail.com>
2017-04-17 19:31:34 +0800

    cookies key? fuck

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Rails.application.config.session_store :cookie_store, key: '_app_session', domain: 'localhost:9080'
-Rails.application.config.session_store :cookie_store, key: '_some_key', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: ENV["COOKIES_SECRET"], domain: :all, tld_length: 2
