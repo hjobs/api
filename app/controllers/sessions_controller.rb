0fca2e9251234689ce46eb4e90b93e934b7893d6
Edmund <edmundtfy@gmail.com>
2017-04-22 11:26:01 +0800

    adding authentication. email should be unique (auth.rb, find_or_create)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,11 +16,23 @@ class SessionsController < ApplicationController
     else
       # Log him in or sign him up
       auth = Auth.find_or_create(auth_hash)
+      
+      # Create JWT
+      @command = AuthenticateUser.call("employee", auth.authable.id, nil)
+
+      unless @command.success?
+        auth.authable.destroy
+        render :json => @command.errors
+        return
+      end
 
       # Create the session
       session[:user_id] = auth.authable.id
 
-      render :json => auth.authable
+      render :json => {
+        user: auth.authable.as_json,
+        token: @command.result
+      }
     end
   end
 
--- app/models/auth.rb
+++ app/models/auth.rb
@@ -4,13 +4,15 @@ class Auth < ApplicationRecord
 
   def self.find_or_create(auth_hash)
     unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-      employee = Employee.create(
-        :name => auth_hash["info"]["name"],
-        :email => auth_hash["info"]["email"],
-        :first_name => auth_hash["info"]["first_name"],
-        :last_name => auth_hash["info"]["last_name"],
-        :pic_sm => auth_hash["info"]["image"]
-      )
+      unless employee = Employee.find_by_email(auth_hash["info"]["email"])
+        employee = Employee.create(
+          :name => auth_hash["info"]["name"],
+          :email => auth_hash["info"]["email"],
+          :first_name => auth_hash["info"]["first_name"],
+          :last_name => auth_hash["info"]["last_name"],
+          :pic_sm => auth_hash["info"]["image"]
+        )
+      end
       auth = create(
         :authable => employee,
         :provider => auth_hash["provider"],
