ffaeb0e3097b8d6241fe3300f7a2f8d04adb2071
Edmund <edmundtfy@gmail.com>
2017-05-16 19:34:55 +0800

    lang_q_serializer renders lang's name as lang_name

--- app/serializers/lang_q_serializer.rb
+++ app/serializers/lang_q_serializer.rb
@@ -1,12 +1,17 @@
 class LangQSerializer < ActiveModel::Serializer
   attributes  :id,
               :lang_code,
+              :lang_name,
               :level
               # :created_at,
               # :updated_at
   
   def lang_code
     object.lang&.code || nil
+  end
+
+  def lang_name
+    object.lang&.name || nil
   end
 
 end
